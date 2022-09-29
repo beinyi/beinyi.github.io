f68272c88582278a17b06053b89553f6e06ca919
beinyi <39291190+beinyi@users.noreply.github.com>
2022-09-29 20:54:47 +0700

    route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
+import {NavBar} from "./components/NavBar/NavBar";
+import {MainWrapper} from "./components/MainWrapper";
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <NavBar/>
+            <MainWrapper />
         </div>
     );
 }
